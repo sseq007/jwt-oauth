7e15a2247771b2313f82c1800cad26d92f30f1aa
sseq007 <sseq007@naver.com>
2023-07-25 00:40:32 +0900

    OAuth 로그인 구현

---
+++ src/main/java/com/junho/oauthwithjwt/global/oauth2/CustomOAuth2User.java
@@ -0,0 +1,23 @@
+package com.junho.oauthwithjwt.global.oauth2;
+
+import com.junho.oauthwithjwt.domain.user.Role;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+public class CustomOAuth2User extends DefaultOAuth2User {
+
+    private String email;
+    private Role role;
+
+
+    public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes, String nameAttributeKey, String email, Role role) {
+        super(authorities, attributes, nameAttributeKey);
+        this.email = email;
+        this.role = role;
+    }
+}
